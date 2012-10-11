8ec4d9c023d78e84333b575533404a9a107f88b3
William Da Cunha <wdacunha77@gmail.com>
2012-10-11 17:16:47 +0200

    Mise en place d'un template Initializr responsive

--- tutorial/cassebrik/js/game.js
+++ tutorial/cassebrik/js/game.js
@@ -1,8 +1,8 @@
 // Constantes du jeu
 var NBR_LIGNES = 5;
-var NBR_BRIQUES_PAR_LIGNE = 8;
-var BRIQUE_WIDTH = 48;
-var BRIQUE_HEIGHT = 15;
+var NBR_BRIQUES_PAR_LIGNE = 10;
+var BRIQUE_WIDTH = 78;
+var BRIQUE_HEIGHT = 30;
 var ESPACE_BRIQUE = 2;
 
 // Variables
