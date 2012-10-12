c5c9b8bbdb555ecfe06990cb3ef4ad9df1f69ac5
William Da Cunha <wdacunha77@gmail.com>
2012-10-12 16:26:41 +0200

    Mise en place du moteur de jeu

--- tutorial/cassebrik/js/game.js
+++ tutorial/cassebrik/js/game.js
@@ -8,12 +8,13 @@
 			}
 			
 			// Initialisation du mur
-			var numberOfLines = 5;
+			var numberOfLines = 4;
 			var brickPerLine = 10;
 			var brickWidth = 78;
 			var brickHeight = 30;
 			var brickSpace = 2;
-			var wall = createWall(context, numberOfLines, brickPerLine, brickWidth, brickHeight, brickSpace);
+			var brickColors = ["#503A22", "#88502F", "#A17048", "#D9C38A", "#F7DDAC"];
+			var wall = createWall(context, numberOfLines, brickPerLine, brickWidth, brickHeight, brickSpace, brickColors);
 			
 			// Initialisation de la barre
 			var barWidth = 160;
@@ -22,37 +23,88 @@
 			var barY = (this.height - barHeight);
 			var barColor = "#333333";
 			var bar = createBar(context, barX, barY, barWidth, barHeight, barColor);
+			
+			// Initialisation du moteur
+			var interval = 10;
+			var game = createMotor(context, wall, bar, this.width, this.height, interval);
+			game.launch();
 		});
 	});
 })(jQuery);
 
-function createWall(ctx, numberOfLines, bricksPerLine, width, height, space) {
+function createWall(ctx, numberOfLines, bricksPerLine, width, height, space, colors) {
 	var wall = new Object()
-	wall.bricks = new Array(numberOfLines);
-	for (var i = 0; i < numberOfLines; i++) {
-		wall[i] = new Array(bricksPerLine);
-		ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
-		for (var j = 0; j < bricksPerLine; j++) {
-			ctx.fillRect((j * (width + space)), (i * (height + space)), width, height);
-			wall[i][j] = 1;
-		}
-	}
 	wall.numberOfLines = numberOfLines;
 	wall.bricksPerLine = bricksPerLine;
 	wall.brick = new Object();
 	wall.brick.width = width;
 	wall.brick.height = height;
 	wall.brick.space = space;
+	wall.colors = colors;
+	wall.bricks = new Array(numberOfLines);
+	wall.reset = function() {
+		for (var i = 0; i < wall.numberOfLines; i++) {
+			wall.bricks[i] = new Array(wall.bricksPerLine);
+			for (var j = 0; j < wall.bricksPerLine; j++) {
+				wall.bricks[i][j] = 1;
+			}
+		}
+	};
+	wall.show = function(ctx) {
+		var width = wall.brick.width;
+		var height = wall.brick.height;
+		var space = wall.brick.space;
+		for (var i = 0; i < wall.numberOfLines; i++) {
+			ctx.fillStyle = wall.colors[i];
+			for (var j = 0; j < wall.bricksPerLine; j++) {
+				if(wall.bricks[i][j] == 1) {
+					ctx.fillRect((j * (width + space)), (i * (height + space)), width, height);
+				}
+			}
+		}
+	};
 	return wall;
 }
 
 function createBar(ctx, x, y, width, height, color) {
 	var bar = new Object();
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, width, height);
 	bar.x = x;
 	bar.y = y;
 	bar.width = width;
 	bar.height = height;
 	bar.color = color;
+	bar.show = function(ctx) {
+		ctx.fillStyle = bar.color;
+		ctx.fillRect(bar.x, bar.y, bar.width, bar.height);
+	};
+	return bar;
+}
+
+function createMotor(ctx, wall, bar, width, height, interval) {
+	var game = new Object();
+	game.context = ctx;
+	game.wall = wall;
+	game.bar = bar;
+	game.width = width;
+	game.height = height;
+	game.interval = interval;
+	game.timer = null;
+	game.launch = function() {
+		wall.reset();
+		game.timer = setTimeout(game.refresh, game.interval);
+	}
+	game.refresh = function() {
+		ctx.save();
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+		ctx.clearRect(0, 0, game.width, game.height);
+		ctx.restore();
+		game.wall.show(game.context);
+		game.bar.show(game.context);
+	};
+	game.end = function() {
+		if(game.timer != null) {
+			clearTimeout(game.timer);
+		}
+	};
+	return game;
 }
