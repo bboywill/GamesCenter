dfdc73bdbdb4499c59f07ce6d8f388eeb5033e53
William Da Cunha <wdacunha77@gmail.com>
2012-10-12 13:52:32 +0200

    Refactoring js et ajout de la barre utilisateur

--- tutorial/cassebrik/js/game.js
+++ tutorial/cassebrik/js/game.js
@@ -1,53 +1,58 @@
-// Constantes du jeu
-var NBR_LIGNES = 5;
-var NBR_BRIQUES_PAR_LIGNE = 10;
-var BRIQUE_WIDTH = 78;
-var BRIQUE_HEIGHT = 30;
-var ESPACE_BRIQUE = 2;
+(function($) {
+	$(document).ready(function() {
+		var elem = $("#game").each(function() {
+			// Initialisation du contexte 2D
+			var context = this.getContext("2d");
+			if (!context) {
+				return;
+			}
+			
+			// Initialisation du mur
+			var numberOfLines = 5;
+			var brickPerLine = 10;
+			var brickWidth = 78;
+			var brickHeight = 30;
+			var brickSpace = 2;
+			var wall = createWall(context, numberOfLines, brickPerLine, brickWidth, brickHeight, brickSpace);
+			
+			// Initialisation de la barre
+			var barWidth = 160;
+			var barHeight = 20;
+			var barX = (this.width / 2) - (barWidth / 2);
+			var barY = (this.height - barHeight);
+			var barColor = "#333333";
+			var bar = createBar(context, barX, barY, barWidth, barHeight, barColor);
+		});
+	});
+})(jQuery);
 
-// Variables
-var tabBriques;
-var context;
-
-window.addEventListener("load", function() {
-	var elem = document.getElementById("game");
-	if (!elem || !elem.getContext) {
-		return;
-	}
-	context = elem.getContext("2d");
-	if (!context) {
-		return;
-	}
-
-	creerBriques(context, NBR_LIGNES, NBR_BRIQUES_PAR_LIGNE, BRIQUE_WIDTH, BRIQUE_HEIGHT, ESPACE_BRIQUE);
-}, false);
-
-// Fonction permettant de créer les briques du jeu
-function creerBriques(ctx, nbrLignes, nbrParLigne, largeur, hauteur, espace) {
-
-	// Tableau virtuel: On initialise les lignes de briques
-	tabBriques = new Array(nbrLignes);
-
-	for (var i = 0; i < nbrLignes; i++) {
-
-		// Tableau virtuel: On initialise les briques de la ligne
-		tabBriques[i] = new Array(nbrParLigne);
-
-		// Affichage: On attribue une couleur aux briques de la ligne
+function createWall(ctx, numberOfLines, bricksPerLine, width, height, space) {
+	var wall = new Object()
+	wall.bricks = new Array(numberOfLines);
+	for (var i = 0; i < numberOfLines; i++) {
+		wall[i] = new Array(bricksPerLine);
 		ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
-
-		for (var j = 0; j < nbrParLigne; j++) {
-
-			// Affichage: On affiche une nouvelle brique
-			ctx.fillRect((j * (largeur + espace)), (i * (hauteur + espace)), largeur, hauteur);
-
-			// Tableau virtuel: On attribue à la case actuelle la valeur 1 = Une
-			// brique existe encore
-			tabBriques[i][j] = 1;
-
+		for (var j = 0; j < bricksPerLine; j++) {
+			ctx.fillRect((j * (width + space)), (i * (height + space)), width, height);
+			wall[i][j] = 1;
 		}
 	}
+	wall.numberOfLines = numberOfLines;
+	wall.bricksPerLine = bricksPerLine;
+	wall.brick = new Object();
+	wall.brick.width = width;
+	wall.brick.height = height;
+	wall.brick.space = space;
+	return wall;
+}
 
-	// Nos briques sont initialisées.
-	return 1;
+function createBar(ctx, x, y, width, height, color) {
+	var bar = new Object();
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, width, height);
+	bar.x = x;
+	bar.y = y;
+	bar.width = width;
+	bar.height = height;
+	bar.color = color;
 }
